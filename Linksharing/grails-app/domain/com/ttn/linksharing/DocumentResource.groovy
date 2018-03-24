496c754d4745704e48653c04a7eb9b474016ca3b
Prachi <prachijulka31@gmail.com>
2018-03-24 16:39:03 +0530

    Question7

--- Linksharing/grails-app/domain/com/ttn/linksharing/DocumentResource.groovy
+++ Linksharing/grails-app/domain/com/ttn/linksharing/DocumentResource.groovy
@@ -1,6 +1,7 @@
 package com.ttn.linksharing
-
+//Document resource should have filepath
 class DocumentResource extends Resource {
+    String url
 
     static hasMany = [subordinates: Resource]
     static constraints = {
