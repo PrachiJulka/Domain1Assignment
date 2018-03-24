57db6a83954a1c1b373a8ae08531012e3cdfb0c4
Prachi <prachijulka31@gmail.com>
2018-03-24 16:37:15 +0530

    Question6

--- Linksharing/grails-app/domain/com/ttn/linksharing/Resource.groovy
+++ Linksharing/grails-app/domain/com/ttn/linksharing/Resource.groovy
@@ -1,5 +1,11 @@
 package com.ttn.linksharing
 
 abstract class Resource {
+    String description
+    Date dateCreated
+    Date lastUpdated
+    static hasMany = [createdBy:User, topics:Topic]
+    static constraints = {
 
+    }
 }
